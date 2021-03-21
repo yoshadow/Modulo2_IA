e52fa6797f1e7793c74ad26b6b2da9f453f504ae
DeskMint <im.shadow21@gmail.com>
2021-03-20 18:52:39 -0600

    Subiendo mi primer archivo en el repo Prueba

---
+++ Programa1.js
@@ -0,0 +1,24 @@
+//Programa de prueba en javascript
+
+// Declarar variables
+var numero = 365;
+
+// Declarar constante
+const mensaje = "Hola mundo";
+
+// Imprimir la variable numero
+console.log(numero);
+numero = 5000;
+console.log(numero);
+
+// Imprimir la constante mensaje
+console.log(mensaje);
+
+// Características de una persona
+const nombre = "Isaac";
+const apePat = "Avlaos";
+const apeMat = "Zuñiga";
+var edad = 30;
+const lugarNac = "México";
+
+// Registro para alumnos = EaXKTBY4
